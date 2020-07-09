a8a613902405729eea0d9d49f9a6ff75c3d5432d
Luis Galocha Dominguez <galdomluis@gmail.com>
2020-07-09 13:26:12 +0200

    termina con IMDB

--- conjunto_datos_IMDB.py
+++ conjunto_datos_IMDB.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import numpy as np
+import matplotlib.pyplot as plt
 from keras.datasets import imdb
 from keras import models
 from keras import layers
@@ -17,6 +18,8 @@ x_train = vectorize_sequences(train_data)
 x_test = vectorize_sequences(test_data)
 y_train = np.asarray(train_labels).astype('float32')
 y_test = np.asarray(test_labels).astype('float32')
+
+'''
 model = models.Sequential()
 model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
 model.add(layers.Dense(16, activation='relu'))
@@ -26,11 +29,13 @@ model.compile(optimizer='rmsprop',
               metrics=['accuracy'])
 
 '''
+'''
 ejemplo de como se hace pasando argumentos o instancias de clases:
 
 model.compile(optimizer=ompitimizer.RMSprop(lr=0.001),
               loss=losses.binary_crossentropy,
               metrics=[metrics.binary_accuracy])
+'''
 '''
 x_val = x_train[:10000]
 partial_x_train = x_train[10000:]
@@ -44,4 +49,48 @@ history = model.fit(partial_x_train,
                     validation_data=(x_val, y_val))
                 
 
+history_dict = history.history
+loss_values = history_dict['loss']
+val_loss_values = history_dict['val_loss']
 
+epochs = range(1, len(loss_values) + 1)
+'''
+'''
+plt.plot(epochs, loss_values, 'bo', label='Training loss')
+plt.plot(epochs, val_loss_values, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.show
+'''
+#plt.clf()
+'''
+acc = history_dict['acc']
+val_acc = history_dict['val_acc']
+
+plt.plot()
+plt.plot(epochs, acc, 'bo', label='Trainig acc')
+plt.plot(epochs, val_acc, 'b', label='Validation accc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+
+plt.show()
+'''
+
+#parte sin sobreajuste
+
+model = models.Sequential()
+model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(16, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=4, batch_size=512)
+results = model.evaluate(x_test, y_test)
